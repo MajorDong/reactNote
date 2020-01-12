406f227ff4aeac3d505f5506af6045cad0e38e45
MajorDong <odsts@outlook.com>
2020-01-13 06:47:01 +0800

    完成井字棋游戏

--- react-demo/src/index.js
+++ react-demo/src/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './App';
+
 import './style.css';
 
 const Cell = function (props) {
@@ -12,34 +12,68 @@ const Cell = function (props) {
 }
 
 const Chessboard = function () {
-  const [cells, setcells] = React.useState([
+  const [cells, setCells] = React.useState([
     [null, null, null],
     [null, null, null],
     [null, null, null],
   ])
   const [n, setN] = React.useState(0)
-  const judge = ()=>{
-    console.log('谁获胜了')
+  const [finshed, setFinshed] = React.useState(false)
+  //这里注意要把新的cells传递进来
+  const judge = (cellsCopy) => {
+    for (let i = 0; i < 3; i++) {
+      if (cellsCopy[i][0] === cellsCopy[i][1] &&
+        cellsCopy[i][1] === cellsCopy[i][2] &&
+        cellsCopy[i][0] !== null
+      ) {
+        console.log(cellsCopy[i][0] + '获胜')
+        setFinshed(true)
+        break
+      }
+    }
+    for (let i = 0; i < 3; i++) {
+      if (cellsCopy[0][i] === cellsCopy[1][i] &&
+        cellsCopy[1][i] === cellsCopy[2][i] &&
+        cellsCopy[0][i] !== null
+      ) {
+        console.log(cellsCopy[0][i] + '获胜')
+        setFinshed(true)
+        break
+      }
+    }
+    if (cellsCopy[0][0] === cellsCopy[1][1] && cellsCopy[1][1] === cellsCopy[2][2]
+      && cellsCopy[0][0] !== null
+    ) {
+      console.log(cellsCopy[0][0] + '获胜')
+      setFinshed(true)
+    }
+    if (cellsCopy[0][2] === cellsCopy[1][1] && cellsCopy[1][1] === cellsCopy[2][0]
+      && cellsCopy[0][2] !== null
+    ) {
+      console.log(cellsCopy[0][2] + '获胜')
+      setFinshed(true)
+    }
   }
-  const onClickCell = (row, col)=>{
+  const onClickCell = (row, col) => {
     //n+2
-    setN(n+1)
+    setN(n + 1)
     //改变点击后的cells
     const cellsCopy = JSON.parse(JSON.stringify(cells))
     cellsCopy[row][col] = n % 2 === 0 ? 'x' : 'o'
-    setcells(cellsCopy)
+    setCells(cellsCopy)
     //判断胜利的一方
-    judge()
-  } 
+    judge(cellsCopy)
+  }
   //注意接受参数又不立即执行
   return (
     <div>
-        <div>n: {n}</div>
-      {cells.map((items,row) => <div className="row">
-        {items.map((item,col) => <div className="cell">
-          <Cell text={item} onClick={()=>onClickCell(row, col)}/>
+      <div>总步数: {n}</div>
+      {cells.map((items, row) => <div className="row">
+        {items.map((item, col) => <div className="cell">
+          <Cell text={item} onClick={() => onClickCell(row, col)} />
         </div>)}
       </div>)}
+      {finshed && <div className="gameOver">游戏结束</div>}
     </div>
   )
 }
